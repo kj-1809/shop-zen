58964876ffe592b7b3174417dbf07736878544ab
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2023-05-31 20:52:06 +0530

    fix : remove elint strict mode for builds

--- next.config.js
+++ next.config.js
@@ -1,15 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images : {
-    remotePatterns : [
-      {
-        protocol : "https",
-        hostname : "uploadthing.com",
-        port : '',
-        pathname : "/f/**"
-      }
-    ]
-  }
-}
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "uploadthing.com",
+				port: "",
+				pathname: "/f/**",
+			},
+		],
+	},
+	eslint: {
+		ignoreDuringBuilds: true,
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
