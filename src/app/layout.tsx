f19886592c38e8b1b583e5dd20dff45ca91f5405
Krishna Jindal <88274406+kj-1809@users.noreply.github.com>
2023-06-01 12:21:45 +0530

    fix : add prisma generate while building

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-	title: "Shop Online",
+	title: "Shop Zen",
 	description: "Buy immaculate products online !",
 };
 
